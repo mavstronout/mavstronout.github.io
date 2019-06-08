a22a263387a963511395e92892d7c69364c59548
mavstronaut <mavbarona@gmail.com>
2019-06-08 12:05:45 -0400

    fixed the dang modals

--- js/main.js
+++ js/main.js
@@ -40,34 +40,34 @@ document.onkeypress = function() {
     
   }
 
-  document.onload = function() {
+  // document.onload = function() {
 
   // $('#projects button').on( 'click', function displayIframe(page) {
   // document.getElementById("iframeDisplay").innerHTML = "<iframe src=\"../pages/pup/${page}.html\" height=\"200\" width=\"300\" ></iframe>";
 
 
   // adding the modal?
-  document.getElementById('btn-modal').addEventListener('click', function() {
-    console.log('clicked b1');
-    document.getElementById('overlay').classList.add('is-visible');
-    // add if/else statements; change modal to the number. 
-    document.getElementById('modal').classList.add('is-visible');
-  });
-};
+//   document.getElementById('btn-modal').addEventListener('click', function() {
+//     console.log('clicked b1');
+//     document.getElementById('overlay').classList.add('is-visible');
+//     // add if/else statements; change modal to the number. 
+//     document.getElementById('modal').classList.add('is-visible');
+//   });
+// };
   
-document.onload = function() {
-  document.getElementById('close-btn').addEventListener('click', function() {
-    document.getElementById('overlay').classList.remove('is-visible');
-    document.getElementById('modal').classList.remove('is-visible');
-  });
-};
+// document.onload = function() {
+//   document.getElementById('close-btn').addEventListener('click', function() {
+//     document.getElementById('overlay').classList.remove('is-visible');
+//     document.getElementById('modal').classList.remove('is-visible');
+//   });
+// };
 
-document.onload = function() {
-  document.getElementById('overlay').addEventListener('click', function() {
-    document.getElementById('overlay').classList.remove('is-visible');
-    document.getElementById('modal').classList.remove('is-visible');
-  });
-};
+// document.onload = function() {
+//   document.getElementById('overlay').addEventListener('click', function() {
+//     document.getElementById('overlay').classList.remove('is-visible');
+//     document.getElementById('modal').classList.remove('is-visible');
+//   });
+// };
   
 
 // });
